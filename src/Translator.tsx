c1f28b31a4e32ba518b869f5884f65d312bf625c
Stanislav Chepiha <stanislav.chepiha@kitrum.com>
2020-09-04 14:29:43 +0200

    Fix multilanguage functionality

--- src/Translator.tsx
+++ src/Translator.tsx
@@ -1,62 +1,54 @@
 import * as React from 'react';
 import { NetInfo } from 'react-native';
 import { connect } from 'react-redux';
-import { I18nextProvider, translate } from 'react-i18next';
+import { withNamespaces } from 'react-i18next';
 import OnBoardingNavigator from './Routes';
 import { PublicSiteStoreState } from './redux/public_site_reducer';
 import { toggleConnection } from './redux/dynamics/dynamics_action_creators';
 import i18n from './i18n';
 
 export interface DispatchProps {
-	onToggleConnection: (onlineState: boolean) => void;
+  onToggleConnection: (onlineState: boolean) => void;
 }
 
 interface Props extends DispatchProps {}
 
 class TranslateStack extends React.Component<Props, {}> {
-	componentDidMount() {
-		NetInfo.isConnected.fetch().then(isConnected => {
-			this.props.onToggleConnection(isConnected);
-		});
-		NetInfo.isConnected.addEventListener('connectionChange', isConnected => this.props.onToggleConnection(isConnected));
-	}
+  componentDidMount() {
+    NetInfo.isConnected.fetch().then((isConnected) => {
+      this.props.onToggleConnection(isConnected);
+    });
+    NetInfo.isConnected.addEventListener('connectionChange', (isConnected) =>
+      this.props.onToggleConnection(isConnected),
+    );
+  }
 
-	render() {
-		return <OnBoardingNavigator screenProps={{ t: i18n.getFixedT('') }} />;
-	}
+  render() {
+    return <OnBoardingNavigator screenProps={{ t: i18n.getFixedT('') }} />;
+  }
 }
 
 function mapStateToProps(state: PublicSiteStoreState) {
-	return {
-		dynamics: state.dynamicsStore.online
-	};
+  return {
+    dynamics: state.dynamicsStore.online,
+  };
 }
 
 function mapDispatchToProps(dispatch: any): DispatchProps {
-	return {
-		onToggleConnection: (onlineState: boolean) => {
-			dispatch(toggleConnection(onlineState));
-		}
-	};
+  return {
+    onToggleConnection: (onlineState: boolean) => {
+      dispatch(toggleConnection(onlineState));
+    },
+  };
 }
 
-const ReloadAppOnLanguageChange = translate('translation', {
-	bindI18n: 'languageChanged',
-	bindStore: 'false'
-})(
-	connect(
-		mapStateToProps,
-		mapDispatchToProps
-	)(TranslateStack)
-);
+const ReloadAppOnLanguageChange = withNamespaces('common', {
+  bindI18n: 'languageChanged',
+  bindStore: 'false',
+})(connect(mapStateToProps, mapDispatchToProps)(TranslateStack));
 
 export default class Translator extends React.Component<{}, {}> {
-	render() {
-		return (
-			// @ts-ignore
-			<I18nextProvider i18n={i18n}>
-				<ReloadAppOnLanguageChange />
-			</I18nextProvider>
-		);
-	}
+  render() {
+    return <ReloadAppOnLanguageChange />;
+  }
 }
