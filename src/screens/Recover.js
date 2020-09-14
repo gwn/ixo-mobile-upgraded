c7799b7a88bd98fee58e7c63d5ae3ec5b7f0ac69
Stanislav Chepiha <stanislav.chepiha@kitrum.com>
2020-09-14 07:10:30 +0200

    Fix Recover navigation

--- src/screens/Recover.js
+++ src/screens/Recover.js
@@ -7,9 +7,18 @@ import {
   Dimensions,
 } from 'react-native';
 import { Container, Icon, Text, View, Content } from 'native-base';
+import SInfo from 'react-native-sensitive-info';
+import { useNavigation } from '@react-navigation/native';
+import { CommonActions } from '@react-navigation/core';
 import { useTranslation } from 'react-i18next';
 
 import { showToast, toastType } from '../utils/toasts';
+import { Encrypt, generateSovrinDID } from '../utils/sovrin';
+import {
+  LocalStorageKeys,
+  SecureStorageKeys,
+  UserStorageKeys,
+} from '../models/phoneStorage';
 
 import DarkButton from '../components/DarkButton';
 import InputField from '../components/InputField';
@@ -21,24 +30,52 @@ import RegisterStyles from '../styles/Register';
 const { width } = Dimensions.get('window');
 import background from '../../assets/background_1.png';
 
-const Recover = () => {
+const Recover = ({ ixo, onUserInit }) => {
   const { t } = useTranslation();
+  const navigation = useNavigation();
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [mnemonic, setMnemonic] = useState('');
 
+  const isLedgered = (did) => {
+    return new Promise((resolve, reject) => {
+      ixo.user
+        .getDidDoc(did)
+        .then((response) => {
+          const { error = false } = response;
+          if (error) {
+            return reject(t('recover:userNotFound'));
+          }
+          return resolve(true);
+        })
+        .catch((error) => {
+          console.log(error);
+          showToast('Error occured', toastType.DANGER);
+        });
+    });
+  };
+
   const handleConfirmMnemonic = async () => {
     try {
-      if (confirmPassword === '' || password === '' || username === '')
-        throw 'register:missingFields';
-      if (password.length < 8) throw 'register:passwordShort';
-      if (password !== confirmPassword) throw 'register:missmatchPassword';
-      if (mnemonic === '') throw 'recover:secretPhrase';
+      if (confirmPassword === '' || password === '' || username === '') {
+        throw t('register:missingFields');
+      }
+      if (password.length < 8) {
+        throw t('register:passwordShort');
+      }
+      if (password !== confirmPassword) {
+        throw t('register:missmatchPassword');
+      }
+      if (mnemonic === '') {
+        throw t('recover:secretPhrase');
+      }
 
       const sovrin = generateSovrinDID(mnemonic);
-      const ledgered = await this.isLedgered('did:sov:' + sovrin.did);
+
+      const ledgered = await isLedgered('did:sov:' + sovrin.did);
+
       if (ledgered) {
         const encryptedMnemonic = Encrypt(
           JSON.stringify({ mnemonic: mnemonic, name: username }),
@@ -52,21 +89,29 @@ const Recover = () => {
         );
 
         SInfo.setItem(SecureStorageKeys.password, password, {});
-        AsyncStorage.setItem(LocalStorageKeys.firstLaunch, 'true'); // stop first time onboarding
+        // AsyncStorage.setItem(LocalStorageKeys.firstLaunch, 'true'); // stop first time onboarding
         const user = {
           did: 'did:sov:' + sovrin.did,
           name: username,
           verifyKey: sovrin.verifyKey,
         };
-        AsyncStorage.setItem(UserStorageKeys.name, user.name);
-        AsyncStorage.setItem(UserStorageKeys.did, user.did);
-        AsyncStorage.setItem(UserStorageKeys.verifyKey, user.verifyKey);
-        this.props.onUserInit(user);
-        this.navigateToLogin();
+        // AsyncStorage.setItem(UserStorageKeys.name, user.name);
+        // AsyncStorage.setItem(UserStorageKeys.did, user.did);
+        // AsyncStorage.setItem(UserStorageKeys.verifyKey, user.verifyKey);
+        onUserInit(user);
+        navigateToLogin();
       }
     } catch (exception) {
       showToast(t(exception), toastType.WARNING);
     }
+  };
+
+  const navigateToLogin = () => {
+    const resetAction = CommonActions.reset({
+      index: 0,
+      routes: [{ name: 'Login' }],
+    });
+    navigation.dispatch(resetAction);
   };
 
   return (
