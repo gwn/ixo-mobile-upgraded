790087f7a3be7995cce7be4261a3adff4336664f
Stanislav Chepiha <stanislav.chepiha@kitrum.com>
2020-09-17 21:48:50 +0200

    Add claims store

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
+
 import { dynamicsReducer } from './dynamics/reducers';
 import { ixoReducer } from './ixo/reducers';
 import { projectReducer } from './projects/reducers';
 import { userReducer } from './user/reducers';
+import { claimsReducer } from './claims/reducers';
 
 export default combineReducers({
-  ixo: ixoReducer,
-  user: userReducer,
+  ixoStore: ixoReducer,
+  userStore: userReducer,
   projectsStore: projectReducer,
   dynamicsStore: dynamicsReducer,
+  claimsStore: claimsReducer,
 });
