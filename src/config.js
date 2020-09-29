11add6d6d7c5ef21b8f70664126e66a5000d0a07
Stanislav Chepiha <stanislav.chepiha@kitrum.com>
2020-09-29 15:26:02 +0200

    Changed HTTPS to HTTP

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ let env = {};
 if (Platform.OS === 'android') {
   env = {
     REACT_APP_BLOCKCHAIN_IP: 'http://androidblockchainmobile.ixo.world',
-    REACT_APP_BLOCK_SYNC_URL: 'https://block_sync_pandora.ixo.world',
+    REACT_APP_BLOCK_SYNC_URL: 'http://block_sync_pandora.ixo.world',
   };
 } else {
   env = {
     REACT_APP_BLOCKCHAIN_IP: 'http://appleblockchainmobile.ixo.world',
-    REACT_APP_BLOCK_SYNC_URL: 'https://block_sync_pandora.ixo.world',
+    REACT_APP_BLOCK_SYNC_URL: 'http://block_sync_pandora.ixo.world',
   };
 }
 
